e16b83928383310354318f0f3b45a26d079905a0
Mandy-Hsiao <412402608@cloud.fju.edu.tw>
2026-09-10 13:59:41 +0800

    middleware執行測試

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,5 @@
+
+
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
@@ -44,3 +46,5 @@ export const config = {
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
+
+
